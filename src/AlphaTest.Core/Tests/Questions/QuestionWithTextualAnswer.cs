ce345b1e23082d5421565203b7b30b9260b774ac
Damakshn <damakshon@gmail.com>
2021-09-03 12:01:52 +0800

    Правильный текстовый ответ не может быть пустым (+ тест)

--- src/AlphaTest.Core/Tests/Questions/QuestionWithTextualAnswer.cs
+++ src/AlphaTest.Core/Tests/Questions/QuestionWithTextualAnswer.cs
@@ -1,11 +1,16 @@
-﻿namespace AlphaTest.Core.Tests.Questions
+﻿using AlphaTest.Core.Tests.Questions.Rules;
+
+namespace AlphaTest.Core.Tests.Questions
 {
     public class QuestionWithTextualAnswer: QuestionWithExactAnswer<string>
     {
         private QuestionWithTextualAnswer() { }
 
         internal QuestionWithTextualAnswer(int testID, QuestionText text, uint number, QuestionScore score, string rightAnswer) :
-            base(testID, text, number, score, rightAnswer){ }
+            base(testID, text, number, score, rightAnswer)
+        {
+            CheckRule(new TextualRightAnswerCannotBeNullOrWhitespaceRule(rightAnswer));
+        }
 
     }
 }
---
+++ src/Tests/AlphaTest.Core.UnitTests/Testmaking/Questions/QuestionWithTextualAnswerTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using AlphaTest.Core.Tests.Questions.Rules;
+
+namespace AlphaTest.Core.UnitTests.Testmaking.Questions
+{
+    
+    public class QuestionWithTextualAnswerTests: QuestionTestsBase
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void CreateQuestionWithTextualAnswer_With_NullOrWhiteSpaceAnswer_IsNotPossible(string rightAnswer)
+        {
+            QuestionTestData data = new() { TextualAnswer = rightAnswer };
+
+            AssertBrokenRule<TextualRightAnswerCannotBeNullOrWhitespaceRule>(() =>
+                CreateQuestionWithTextualAnswer(data)
+            );
+        }
+    }
+}
