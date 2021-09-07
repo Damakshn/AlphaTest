8c6ddb13d69b2c5a1551d7e4af1d131314304017
Damakshn <damakshon@gmail.com>
2021-09-07 14:57:54 +0800

    Реализован метод назначения заявки администратору.

---
+++ src/AlphaTest.Core/Tests/Publishing/Rules/ProposalCanBeAssignedOnlyToAdminUsersRule.cs
@@ -0,0 +1,19 @@
+﻿using AlphaTest.Core.Common;
+using AlphaTest.Core.Users;
+
+namespace AlphaTest.Core.Tests.Publishing.Rules
+{
+    public class ProposalCanBeAssignedOnlyToAdminUsersRule : IBusinessRule
+    {
+        private readonly User _user;
+
+        public ProposalCanBeAssignedOnlyToAdminUsersRule(User user)
+        {
+            _user = user;
+        }
+
+        public string Message => "Заявку может обработать только администратор.";
+
+        public bool IsBroken => _user.IsInRole(UserRole.ADMIN) == false;
+    }
+}
