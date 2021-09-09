2a0e926d545c8f0bbe6cb25d5041504c6a70940d
Damakshn <damakshon@gmail.com>
2021-09-09 14:32:10 +0800

    Составителей теперь можно копировать, тест написан. В тесты учёта составителей добавлена проверка для успешного добавления составителя.

--- src/AlphaTest.Core/Tests/Ownership/Contribution.cs
+++ src/AlphaTest.Core/Tests/Ownership/Contribution.cs
@@ -3,6 +3,7 @@ using AlphaTest.Core.Users;
 using AlphaTest.Core.Tests.Ownership.Rules;
 namespace AlphaTest.Core.Tests.Ownership
 {
+    // MAYBE положить в один агрегат вместе с Test
     public class Contribution: Entity
     {
         public Contribution(Test test, User teacher)
@@ -11,6 +12,13 @@ namespace AlphaTest.Core.Tests.Ownership
             CheckRule(new SuspendedUserCannotBeSetAsNewAuthorOrContributorRule(teacher));
             TestID = test.ID;
             TeacherID = teacher.ID;
+        }
+
+        public Contribution ReplicateForNewEdition(Test newEdition)
+        {
+            Contribution replica = (Contribution)this.MemberwiseClone();
+            replica.TestID = newEdition.ID;
+            return replica;
         }
 
         public int TestID { get; private set; }
