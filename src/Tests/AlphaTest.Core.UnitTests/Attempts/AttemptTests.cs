6a6e7699295f80bf4aa547ec00322b8d3284d468
Damakshn <damakshon@gmail.com>
2021-09-29 13:25:57 +0800

    Тесты для попыток переведены на AutoFixture.

--- src/Tests/AlphaTest.Core.UnitTests/Attempts/AttemptTests.cs
+++ src/Tests/AlphaTest.Core.UnitTests/Attempts/AttemptTests.cs
@@ -1,169 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 using Moq;
-using AlphaTest.Core.UnitTests.Common;
-using System.Collections.Generic;
-using AlphaTest.Core.Examinations;
-using AlphaTest.Core.UnitTests.Common.Helpers;
-using AlphaTest.Core.Tests;
 using AlphaTest.Core.Attempts;
 using AlphaTest.Core.Attempts.Rules;
-using AlphaTest.Core.UnitTests.Examinations;
+using AlphaTest.Core.Tests;
+using AlphaTest.Core.Examinations;
+using AlphaTest.Core.UnitTests.Common;
+using AlphaTest.Core.UnitTests.Fixtures;
+using AlphaTest.Core.UnitTests.Common.Helpers;
 
 namespace AlphaTest.Core.UnitTests.Attempts
 {
     public class AttemptTests: UnitTestBase
     {
-        [Fact]
-        public void New_attempt_cannot_be_started_for_canceled_examination()
+        [Theory, AttemptTestsData]
+        public void New_attempt_cannot_be_started_for_canceled_examination(Test test, Examination examination)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-
             examination.Cancel();
 
             AssertBrokenRule<NewAttemptCannotBeStartedIfExamIsClosedRule>(() => 
-                new Attempt(data.Test, examination, It.IsAny<int>()));
+                new Attempt(test, examination, It.IsAny<int>()));
         }
 
-        [Fact]
-        public void New_attempt_cannot_be_started_if_examination_is_already_ended()
+        [Theory, AttemptTestsData]
+        public void New_attempt_cannot_be_started_if_examination_is_already_ended(Test test, Examination examination)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-
             HelpersForExaminations.SetExaminationDates(examination, DateTime.Now.AddDays(-25), DateTime.Now.AddDays(-10));
 
             AssertBrokenRule<NewAttemptCannotBeStartedIfExaminationIsAreadyEndedRule>(() =>
-                new Attempt(data.Test, examination, It.IsAny<int>()));
+                new Attempt(test, examination, It.IsAny<int>()));
         }
 
-        [Fact]
-        public void New_attempt_can_be_started()
+        [Theory, AttemptTestsData]
+        public void New_attempt_can_be_started(Test test, Examination examination)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
+            Attempt attempt = new(test, examination, It.IsAny<int>());
 
             Assert.Equal(examination.ID, attempt.ExaminationID);
             Assert.Equal(DateTime.Now, attempt.StartedAt, TimeSpan.FromSeconds(1));
         }
 
         [Theory]
-        [MemberData(nameof(TimeLimits))]
+        [AttemptTestsMemberData(nameof(TimeLimits))]
         public void Actual_time_limit_is_equal_to_minimum_between_time_limit_for_test_and_remained_time_of_examination(
             TimeSpan timeLimitForTest,
             TimeSpan examTimeRemained,
-            TimeSpan expected)
+            TimeSpan expected,
+            Test test,
+            Examination examination)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            HelpersForTests.SetNewStatusForTest(data.Test, TestStatus.Draft);
-            data.Test.ChangeTimeLimit(timeLimitForTest);
-            HelpersForTests.SetNewStatusForTest(data.Test, TestStatus.Published);
-            Examination examination = HelpersForExaminations.CreateExamination(data);
+            test.ChangeTimeLimit(timeLimitForTest);
+
+            // сроки экзамена приходится ломать через рефлексию, в естественных условиях
+            // такая ситуация (когда экзамен вот-вот закончится) возникает сама собой
             HelpersForExaminations.SetExaminationDates(
                 examination,
                 DateTime.Now,
                 DateTime.Now + examTimeRemained
             );
 
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
+            Attempt attempt = new(test, examination, It.IsAny<int>());
 
             Assert.Equal(DateTime.Now + expected, attempt.ForceEndAt, TimeSpan.FromSeconds(10));
         }
 
-        [Fact]
-        public void Attempt_can_be_finished()
-        {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
 
+        [Theory, AttemptTestsData]
+        public void Attempt_can_be_finished(Attempt attempt)
+        {
             attempt.Finish();
 
             Assert.True(attempt.IsFinished);
             Assert.Equal(DateTime.Now, (DateTime)attempt.FinishedAt, TimeSpan.FromSeconds(1));
         }
 
-        [Fact]
-        public void Attempt_can_be_finished_only_once()
+        [Theory, AttemptTestsData]
+        public void Attempt_can_be_finished_only_once(Attempt attempt)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
             attempt.Finish();
-
             AssertBrokenRule<FinishedAttemptCannotBeModifiedRule>(() => attempt.Finish());
         }
 
-        [Fact]
-        public void Attempt_can_be_finished_by_force()
+        [Theory, AttemptTestsData]
+        public void Attempt_can_be_finished_by_force(Attempt attempt)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
             HelpersForAttempts.SetAttemptForcedEndDate(attempt, DateTime.Now);
 
             attempt.ForceEnd();
 
             Assert.True(attempt.IsFinished);
-            
         }
 
-        [Fact]
-        public void Attempt_cannot_be_finished_by_force_if_time_limit_is_not_expired()
+        [Theory, AttemptTestsData]
+        public void Attempt_cannot_be_finished_by_force_if_time_limit_is_not_expired(Attempt attempt)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
-
             AssertBrokenRule<ForcedEndMustBeAppliedAtRightTimeRule>(() => attempt.ForceEnd());
         }
 
-        [Fact]
-        public void Attempt_cannot_be_finished_by_force_if_it_is_already_finished()
+        [Theory, AttemptTestsData]
+        public void Attempt_cannot_be_finished_by_force_if_it_is_already_finished(Attempt attempt)
         {
-            ExaminationTestData data = new()
-            {
-                StartsAt = DateTime.Now.AddDays(1),
-                EndsAt = DateTime.Now.AddDays(10)
-            };
-            Examination examination = HelpersForExaminations.CreateExamination(data);
-            Attempt attempt = new(data.Test, examination, It.IsAny<int>());
             HelpersForAttempts.SetAttemptForcedEndDate(attempt, DateTime.Now);
+
             attempt.Finish();
 
             AssertBrokenRule<FinishedAttemptCannotBeModifiedRule>(() => attempt.ForceEnd());
---
+++ src/Tests/AlphaTest.Core.UnitTests/Fixtures/AttemptTestsDataAttribute.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using Xunit.Sdk;
+
+namespace AlphaTest.Core.UnitTests.Fixtures
+{
+    internal class AttemptTestsDataAttribute: AutoDataAttribute
+    {
+        public AttemptTestsDataAttribute()
+            :base(() => new Fixture().Customize(new AttemptTestingCustomization()))
+        {
+
+        }
+    }
+
+    internal class AttemptTestsMemberDataAttribute : MemberAutoDataAttribute
+    {
+        public AttemptTestsMemberDataAttribute(string memberName, params object[] parameters)
+            :base(new AttemptTestsDataAttribute(), memberName, parameters)
+        {
+
+        }
+    }
+
+
+}
