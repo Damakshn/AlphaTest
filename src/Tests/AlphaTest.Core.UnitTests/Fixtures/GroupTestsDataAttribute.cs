80637a820233ba810e328aed385965b8aa69c600
Damakshn <damakshon@gmail.com>
2021-09-29 14:34:18 +0800

    Частичный перевод тестов для групп на Autofixture.

---
+++ src/Tests/AlphaTest.Core.UnitTests/Fixtures/GroupTestsDataAttribute.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+using AutoFixture.Xunit2;
+using AlphaTest.Core.UnitTests.Fixtures.Groups;
+using System;
+
+namespace AlphaTest.Core.UnitTests.Fixtures
+{
+    internal class GroupTestsDataAttribute: AutoDataAttribute
+    {
+        public GroupTestsDataAttribute()
+            : base(() => new Fixture().Customize(new DefaultGroupCustomization()))
+        {
+
+        }
+    }
+
+
+    internal class GroupTestsMemberDataAttribute : MemberAutoDataAttribute
+    {
+        public GroupTestsMemberDataAttribute(string memberName, params object[] parameters)
+            : base(new GroupTestsDataAttribute(), memberName, parameters)
+        {
+
+        }
+    }
+
+    internal class GroupTestsInlineDataAttribute : InlineAutoDataAttribute
+    {
+        public GroupTestsInlineDataAttribute(params object[] values)
+            :base(new GroupTestsDataAttribute(), values)
+        {
+                
+        }
+    }
+
+    
+}
---
+++ src/Tests/AlphaTest.Core.UnitTests/Fixtures/Groups/DefaultGroupCustomization.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AutoFixture;
+using AlphaTest.Core.Groups;
+
+
+namespace AlphaTest.Core.UnitTests.Fixtures.Groups
+{
+    internal class DefaultGroupCustomization : ICustomization
+    {
+        public void Customize(IFixture fixture)
+        {
+            fixture.Customize<Group>(
+                c => c.FromFactory(
+                    (string groupName) => new Group(groupName, DateTime.Now.AddDays(1), DateTime.Now.AddDays(100), false)
+                )
+            );
+        }
+    }
+}
