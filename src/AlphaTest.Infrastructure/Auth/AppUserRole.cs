83862cd39573a77ffe9e0fc9f72d19e94946f86d
Damakshn <damakshon@gmail.com>
2021-10-14 14:52:06 +0800

    Настройка Identity - новый конструктор для модели пользователя, кастомизированная роль; Добавлен метод для загрузки агрегата пользователя со всеми ролями. Обновление пакетов для работы с БД.

---
+++ src/AlphaTest.Infrastructure/Auth/AppUserRole.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace AlphaTest.Infrastructure.Auth
+{
+    public class AppUserRole : IdentityUserRole<Guid>
+    {
+        public AppUser User { get; set; }
+
+        public AppRole Role {get; set;}
+    }
+}
