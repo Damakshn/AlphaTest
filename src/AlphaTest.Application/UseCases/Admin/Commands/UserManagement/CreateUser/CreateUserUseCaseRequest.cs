f4c76ec26cf6a15824808ff2a35aa0fcb7041981
Damakshn <damakshon@gmail.com>
2021-11-01 19:42:48 +0800

    Правки к команде создания пользователя - теперь есть проверка, не занят ли email. Убраны лишние using'и

--- src/AlphaTest.Application/UseCases/Admin/Commands/UserManagement/CreateUser/CreateUserUseCaseRequest.cs
+++ src/AlphaTest.Application/UseCases/Admin/Commands/UserManagement/CreateUser/CreateUserUseCaseRequest.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using AlphaTest.Application.UseCases.Common;
 
 namespace AlphaTest.Application.UseCases.Admin.Commands.UserManagement.CreateUser
