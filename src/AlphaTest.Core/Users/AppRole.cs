d2d9df200ddbe36b5264cde4456b9d3ce2701569
Damakshn <damakshon@gmail.com>
2022-01-05 18:16:49 +0800

    Класс пользователя, основанный на Identity, перенесён в ядро системы.

---
+++ src/AlphaTest.Core/Users/AppRole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+
+namespace AlphaTest.Core.Users
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        public AppRole(string name, string nameInRussian) : base()
+        {
+            Name = name;
+            NormalizedName = name.ToUpper();
+            NameInRussian = nameInRussian;
+        }
+
+        public string NameInRussian { get; set; }
+    }
+}
---
+++ src/AlphaTest.Core/Users/AppUserRole.cs
@@ -0,0 +1,9 @@
+﻿namespace AlphaTest.Core.Users
+{
+    public class AppUserRole
+    {
+        public AppUser User { get; set; }
+
+        public AppRole Role { get; set; }
+    }
+}
