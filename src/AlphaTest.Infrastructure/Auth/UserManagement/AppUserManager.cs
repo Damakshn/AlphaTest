f59a14477c4566cd648ffc3332f7f9cbe18005c9
Damakshn <damakshon@gmail.com>
2022-01-05 18:25:55 +0800

    AppUserStore & AppUserManager переориентированы на класс пользователя из ядра.

--- src/AlphaTest.Infrastructure/Auth/UserManagement/AppUserManager.cs
+++ src/AlphaTest.Infrastructure/Auth/UserManagement/AppUserManager.cs
@@ -4,16 +4,17 @@ using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AlphaTest.Core.Users;
 
 namespace AlphaTest.Infrastructure.Auth.UserManagement
 {
-    public class AppUserManager : UserManager<AppUser>
+    public class AppUserManager : UserManager<AlphaTestUser>
     {
-        public AppUserManager(IUserStore<AppUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<AppUser> passwordHasher, IEnumerable<IUserValidator<AppUser>> userValidators, IEnumerable<IPasswordValidator<AppUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<AppUser>> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        public AppUserManager(IUserStore<AlphaTestUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<AlphaTestUser> passwordHasher, IEnumerable<IUserValidator<AlphaTestUser>> userValidators, IEnumerable<IPasswordValidator<AlphaTestUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<AlphaTestUser>> logger) : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
         {
         }
 
-        public Task<AppUser> FindByIdAsync(Guid userId)
+        public Task<AlphaTestUser> FindByIdAsync(Guid userId)
         {
             ThrowIfDisposed();
             return (Store as AppUserStore).FindByIdAsync(userId, CancellationToken);
--- src/AlphaTest.Infrastructure/Auth/UserManagement/AppUserStore.cs
+++ src/AlphaTest.Infrastructure/Auth/UserManagement/AppUserStore.cs
@@ -6,17 +6,18 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using AlphaTest.Infrastructure.Database;
 using AlphaTest.Infrastructure.Database.QueryExtensions;
+using AlphaTest.Core.Users;
 
 namespace AlphaTest.Infrastructure.Auth.UserManagement
 {
-    public class AppUserStore : UserStore<AppUser, AppRole, AlphaTestContext, Guid, IdentityUserClaim<Guid>, AppUserRole, IdentityUserLogin<Guid>, IdentityUserToken<Guid>, IdentityRoleClaim<Guid>>
+    public class AppUserStore : UserStore<AlphaTestUser, AppRole, AlphaTestContext, Guid, IdentityUserClaim<Guid>, AppUserRole, IdentityUserLogin<Guid>, IdentityUserToken<Guid>, IdentityRoleClaim<Guid>>
     {
         public AppUserStore(AlphaTestContext context, IdentityErrorDescriber describer = null) : base(context, describer)
         {
         }
 
         #region Методы поиска пользователей
-        public override Task<AppUser> FindByIdAsync(string userId, CancellationToken cancellationToken = default)
+        public override Task<AlphaTestUser> FindByIdAsync(string userId, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
             ThrowIfDisposed();
@@ -24,21 +25,21 @@ namespace AlphaTest.Infrastructure.Auth.UserManagement
             return Context.Users.Aggregates().FirstOrDefaultAsync(u => u.Id == id, cancellationToken);
         }
 
-        public Task<AppUser> FindByIdAsync(Guid userId, CancellationToken cancellationToken = default)
+        public Task<AlphaTestUser> FindByIdAsync(Guid userId, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
             ThrowIfDisposed();
             return Context.Users.Aggregates().FirstOrDefaultAsync(u => u.Id == userId, cancellationToken);
         }
 
-        public override Task<AppUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken = default)
+        public override Task<AlphaTestUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
             ThrowIfDisposed();
             return Context.Users.Aggregates().FirstOrDefaultAsync(u => u.NormalizedUserName == normalizedUserName, cancellationToken);
         }
 
-        public override Task<AppUser> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken = default)
+        public override Task<AlphaTestUser> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken = default)
         {
             cancellationToken.ThrowIfCancellationRequested();
             ThrowIfDisposed();
@@ -46,7 +47,7 @@ namespace AlphaTest.Infrastructure.Auth.UserManagement
             return Context.Users.Aggregates().SingleOrDefaultAsync(u => u.NormalizedEmail == normalizedEmail, cancellationToken);
         }
 
-        protected override Task<AppUser> FindUserAsync(Guid userId, CancellationToken cancellationToken)
+        protected override Task<AlphaTestUser> FindUserAsync(Guid userId, CancellationToken cancellationToken)
         {
             return Context.Users.Aggregates().SingleOrDefaultAsync(u => u.Id.Equals(userId), cancellationToken);
         }
