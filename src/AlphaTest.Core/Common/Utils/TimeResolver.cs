8da127bc21ef828230e8d4b82cf2a9e21439bc6b
Damakshn <damakshon@gmail.com>
2022-01-14 16:35:01 +0800

    Добавлен класс TimeResolver для определения текущего времени.

---
+++ src/AlphaTest.Core/Common/Utils/TimeResolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaTest.Core.Common.Utils
+{
+    public static class TimeResolver
+    {
+        private static DateTime? _customizedTime;
+
+        public static void SetTime(DateTime customizedTime)
+        {
+            _customizedTime = customizedTime;
+        }
+
+        public static void ResetTime()
+        {
+            _customizedTime = null;
+        }
+
+        public static DateTime CurrentTime => _customizedTime.HasValue ? DateTime.UtcNow : _customizedTime.Value;
+    }
+}
