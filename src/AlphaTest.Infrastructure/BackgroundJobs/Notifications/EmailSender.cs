65c8a0909e92bbc56e6264865ef7f4679f5f0748
Damakshn <damakshon@gmail.com>
2022-01-24 15:33:03 +0800

    Добавлена заготовка для сервиса

---
+++ src/AlphaTest.Infrastructure/BackgroundJobs/Notifications/EmailSender.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AlphaTest.Application.Notifications;
+using Microsoft.Extensions.Hosting;
+
+namespace AlphaTest.Infrastructure.BackgroundJobs.Notifications
+{
+    public class EmailSender : IHostedService, IEmailSender
+    {
+        public Task SendBroadcastNotificationAsync(IBroadcastNotification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendIndividualNotificationAsync(IIndividualNotification notification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
